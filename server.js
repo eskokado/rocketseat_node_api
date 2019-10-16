c36663e5a2cb355a98eb1bbcbc8a002860de2735
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-16 20:21:36 -0300

    Criando model de Produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // iniciando o APP
 const app = express();
@@ -7,10 +8,19 @@ const app = express();
 // Iniciando o DB
 //mongoose.connect('mongodb://localhost:27017/nodeapi');
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+//require('./src/models/Product');
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
 app.get('/', (req, res) => {
-    res.send("Hello Eskokado");
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native',
+    });
+    return res.send("Hello Eskokado");
 });
 
 app.listen(3001);
