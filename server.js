eccf0bfe2bce58032e4ef8629b3f1a3b83720c71
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-16 19:49:11 -0300

    Conectando Database

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// iniciando o APP
 const app = express();
 
+// Iniciando o DB
+//mongoose.connect('mongodb://localhost:27017/nodeapi');
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
+
+// Primeira rota
 app.get('/', (req, res) => {
     res.send("Hello Eskokado");
 });
