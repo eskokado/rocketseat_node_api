aa057606c6c54e111ed377303576b3cbdf253f4b
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-16 21:13:05 -0300

    Criação de registro

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const requireDir = require('require-dir');
 
 // iniciando o APP
 const app = express();
+app.use(express.json());
 
 // Iniciando o DB
 //mongoose.connect('mongodb://localhost:27017/nodeapi');
 mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true });
 //require('./src/models/Product');
 requireDir('./src/models');
+
 
 // Rotas
 app.use('/api', require('./src/routes'));
